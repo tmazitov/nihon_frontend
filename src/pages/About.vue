7c9a26e82e5680c72e5a7bb503e62498d4d235f2
Timur Mazitov <timurmazitov000@gmail.com>
2022-10-24 11:27:11 +0300

    add content to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="about__page">
-        <h1>
-            Nihon
-        </h1>
+    <div class="fcc about__page">
+        <div class="fcc title__cont card">
+            <div class="title">Nihon</div>
+            <div class="descriprion">
+                Application for learning and repeating words. Test your knowledge of words
+            </div>
+        </div>
+
+        <div class="card">
+            Write down new words to record the process of learning a language
+        </div>
+
+        <div class="card">
+            Compete with your friends in the knowledge of words
+        </div>
     </div>
 </template>
 
@@ -11,3 +22,23 @@ export default {
     name: 'AboutPage',
 }
 </script>
+
+<style scoped>
+.card{
+    border-radius: 7px;
+    background: #e1e1e1;
+    margin-top: 32px;
+    width: 700px;
+    padding: 15px;
+}
+.about__page{
+    flex-direction: column;
+}
+.title__cont{
+    gap: 15px;
+}
+
+.title{
+    font-size: 96px
+}
+</style>
